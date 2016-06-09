c0e006d12083d1726190f6da29781d0516bb66f1
PaulPearman <paulpearman@residentrealty.com>
2016-06-09 16:09:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var main = function() {
+   var SlideLength = 750;
+   var SlideSpeed = 1000;
+   var Pause = 3000;
+   var currentSlide = 1;
+   
+   var $PhotoReel = $('.PhotoReel');
+   var $Photo = $PhotoReel.find('.Photo');
+   
    setInterval(function() {
-   $('.PhotoReel').animate({'margin-left': '-=750px'}, 1000); 
-   }, 3000);
+   $PhotoReel.animate({'margin-left': '-='+SlideLength}, SlideSpeed, function() {
+    currentSlide++;
+    if (currentSlide === $Photo.length) {
+       CurrentSlide = 1;
+       $PhotoReel.css('margin-left', 0);
+         }
+      }
+   }, Pause); 
 };
 $(document).ready(main);
